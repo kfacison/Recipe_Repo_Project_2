718b91091a5a2e520161115a5be8830fbee20d54
kfac <kfacison1998@gmail.com>
2023-10-27 17:31:33 -0500

    add all models

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ingredientSchema = new Schema({
+    foodCategory: {
+        type: String,
+        enum: ['Grains','Vegetables','Fruits','Proteins','Dairy','Fats and Oils','Beverages','Herbs and Spices','Misc.']
+    }
+}, {
+    timestamps: true
+});
+
 module.exports = mongoose.model('Ingredient', );
--- models/recipe.js
+++ models/recipe.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema;
 const recipeSchema = new Schema({
     mealType: {
         type: String,
-        enum: ['Breakfast','Lunch','Dinner','Snack']
+        enum: ['Breakfast','Lunch','Dinner','Snack','Misc.']
     },
     cuisineType:{
         type: String,
-        enum: ['Italian','Mexican','Chinese','Indian','Japanese','Thai','French','Greek','Spanish','Korean','Lebanese','Vietnamese','Mediterranean','Brazilian','German','Turkish','Moroccan','Peruvian','Russian','Caribbean']
+        enum: ['Italian','Mexican','Chinese','Indian','Japanese','Thai','French','Greek','Spanish','Korean','Lebanese','Vietnamese','Mediterranean','Brazilian','German','Turkish','Moroccan','Peruvian','Russian','Caribbean','American','Misc.']
     },
     cookTime: Number,
     ingredientList: {

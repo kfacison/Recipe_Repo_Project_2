9b1a8eb7dad530c1692659e70e5493b87f09ace3
kfac <kfacison1998@gmail.com>
2023-10-30 23:21:11 -0500

    adding recipe show view

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -20,29 +20,33 @@ async function newRecipeForm(req, res){
 }
 
 async function show(req, res){
-
-    res.render('recipes/show', {title:"Recipe"})
+    const recipe = await Recipe.findById(req.params);
+    console.log(req.params);
+    res.render('recipes/show', {title:"Recipe", recipe})
 }
 
 async function create(req, res){
+    //all ingredients in the data base name and id
     const allingredients = await Ingredient.find({},{name: 1, _id:0});
-    const ingList = allingredients.map(i => i.name);
+    //make into an array of just names
+    //const ingList = allingredients.map(i => i.name);
+    //takes off spaces on string of the ingredents
     req.body.ingredientList = req.body.ingredientList.trim();
+    //makes an arrray seperated by commas
     req.body.ingredientList = req.body.ingredientList.split(/\s*,\s*/);
-    console.log(ingList);
+    //console.log(ingList);
+
     const finalList = [];
-    for(let ing of req.body.ingredientList){
-        if(ingList.includes(ing)===false){
-            console.log(ing)
-            const newIng = await Ingredient.create({name: ing, foodCategory: 'Misc.'});
-            finalList.push(newIng._id);
-            console.log(finalList)
+    for(let j=0; j<req.body.ingredientList.length;j++){
+        for(let i=0; i<allingredients.length;i++){
+            if(allingredients[i].name===j){
+                finalList.push(allingredients[i]._id);
+                console.log(finalList)
+            }
         }
-        else{
-            const newIng = await Ingredient.find({name: ing});
-            finalList.push(newIng._id);
-            console.log(finalList)
-        }
+        const newIng = await Ingredient.create({name: req.body.ingredientList[j], foodCategory: 'Misc.'});
+        finalList.push(newIng._id);
+        console.log(finalList)
     }
     req.body.ingredientList = finalList;
     req.body.chef = req.user._id;
@@ -53,6 +57,6 @@ async function create(req, res){
     }
     catch (err){
         console.log(err);
-        res.redirect('/recipes/:recipesId', { errorMsg: err.message });
+        res.redirect(`/recipes/new`, { errorMsg: err.message });
     }
 }
